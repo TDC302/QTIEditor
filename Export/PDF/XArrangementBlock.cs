0c8637a91aaad68091b930df41c3e11b4560426c
Tdc302 <tdc302@gmail.com>
2025-04-15 17:00:34 -0400

    Created class for arranging groups of items

---
+++ Export/PDF/XArrangementBlock.cs
@@ -0,0 +1,54 @@
+﻿using PdfSharp.Drawing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QTIEditor.Export.PDF
+{
+
+    /// <summary>
+    /// Groups graphics objects together so that they can be reused or logically divided
+    /// </summary>
+    internal class XArrangementBlock : List<IDrawableBlock>, IDrawableBlock
+    {
+        public double Width
+        {
+            get
+            {
+                double edgeDist = 0;
+                foreach (IDrawableBlock block in this)
+                {
+                    edgeDist = double.Max(edgeDist, block.Position.X + block.Width);
+                }
+                return edgeDist;
+            }
+        }
+        public double Height
+        {
+            get {
+                double edgeDist = 0;
+                foreach (IDrawableBlock block in this)
+                {
+                    edgeDist = double.Max(edgeDist, block.Position.Y + block.Height);
+                }
+                return edgeDist;
+            }
+        }
+
+
+        public XPoint Position { get; set; }
+
+
+        public void Draw(XGraphics graphics)
+        {
+            foreach (IDrawableBlock item in this)
+            {
+                item.Position.Offset(Position.X, Position.Y);
+                item.Draw(graphics);
+                item.Position.Offset(-Position.X, -Position.Y);
+            }
+        }
+    }
+}
